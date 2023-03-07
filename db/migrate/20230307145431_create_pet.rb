b099f6bac1c18079d24edaf34bf35e2e7c7aec50
kidhri salim <salimkhidhri@gmail.com>
2023-03-07 17:58:52 +0300

    added migration

--- db/migrate/20230307145431_create_pet.rb
+++ db/migrate/20230307145431_create_pet.rb
@@ -1,4 +1,11 @@
 class CreatePet < ActiveRecord::Migration[6.1]
   def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :breed
+      t.string :image_url
+      t.integer :user_id
+      t.timestamps
+    end
   end
 end
